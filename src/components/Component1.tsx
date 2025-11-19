fc978786ba848ef4b802118814a37ad73ed20b7b
ZaraQureshi <qzara819@gmail.com>
2025-11-19 23:58:33 +0530

    add full width to progressbar

--- src/components/Component1.tsx
+++ src/components/Component1.tsx
@@ -2,23 +2,24 @@ import { useSelector } from "react-redux"
 
 export default function Component1() {
     const count = useSelector((state: any) => state.counter.value);
-    console.log(count.red+count.green+count.blue)
+    console.log("total:",(count.red+count.green+count.blue)/3*100)
+    const total=count.red+count.green+count.blue;
 
     return (
         <>
             <div className="border-2 border-amber-700">
                 <p>Score:</p>
-                <div className="flex">
+                <div className="flex w-full" style={{}}>
 
-                <span style={{width:count.red/10+'%',display:`block`,background:'red'}}>x</span>
-                <span style={{width:count.green/10+'%',display:`block`,background:'green'}}>x</span>
-                <span style={{width:count.blue/10+'%',display:`block`,background:'blue'}}>x</span>
+                <span className="transition-all duration-500 ease-out" style={{width:(count.red/total)*100+'%',display:`block`,background:'red'}}>{count.red/10+'%'}</span>
+                <span className="transition-all duration-500 ease-out" style={{width:(count.green/total)*100+'%',display:`block`,background:'green'}}>{count.green/10+'%'}</span>
+                <span className="transition-all duration-500 ease-out" style={{width:(count.blue/total)*100+'%',display:`block`,background:'blue'}}>{count.blue/10+'%'}</span>
                 </div>
 
                 <div className="flex justify-evenly">
-                    <span>Red:{count.red}</span>
-                    <span>Green:{count.green}</span>
-                    <span>Blue:{count.blue}</span>
+                    <span>Red:{count.red/10}</span>
+                    <span>Green:{count.green/10}</span>
+                    <span>Blue:{count.blue/10}</span>
                 </div>
 
             </div>
