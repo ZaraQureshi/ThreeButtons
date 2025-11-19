d1bba9c04012ee3e0270fba7c0596eb2d08acac9
ZaraQureshi <qzara819@gmail.com>
2025-11-20 00:15:32 +0530

    this looks good

--- src/components/Component1.tsx
+++ src/components/Component1.tsx
@@ -4,6 +4,9 @@ export default function Component1() {
     const count = useSelector((state: any) => state.counter.value);
     console.log("total:",(count.red+count.green+count.blue)/3*100)
     const total=count.red+count.green+count.blue;
+    const red:number=Math.round((count.red/total)*100) ||0;
+    const green:number=Math.round((count.green/total)*100) ||0;
+    const blue:number=Math.round((count.blue/total)*100)||0;
 
     return (
         <>
@@ -11,9 +14,9 @@ export default function Component1() {
                 <p>Score:</p>
                 <div className="flex w-full" style={{}}>
 
-                <span className="transition-all duration-500 ease-out" style={{width:(count.red/total)*100+'%',display:`block`,background:'red'}}>{count.red/10+'%'}</span>
-                <span className="transition-all duration-500 ease-out" style={{width:(count.green/total)*100+'%',display:`block`,background:'green'}}>{count.green/10+'%'}</span>
-                <span className="transition-all duration-500 ease-out" style={{width:(count.blue/total)*100+'%',display:`block`,background:'blue'}}>{count.blue/10+'%'}</span>
+                <span className="transition-all duration-500 ease-out" style={{width:red+'%',display:`block`,background:'red'}}>{red?red+'%':""}</span>
+                <span className="transition-all duration-500 ease-out" style={{width:green+'%',display:`block`,background:'green'}}>{green?green+'%':""}</span>
+                <span className="transition-all duration-500 ease-out" style={{width:blue+'%',display:`block`,background:'blue'}}>{blue?blue+'%':""}</span>
                 </div>
 
                 <div className="flex justify-evenly">
